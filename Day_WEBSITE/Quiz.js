59c0fbd8d294becb2fc71082b422ea39391803ec
FunnySkillz <sebastijan.bogdan@outlook.com>
2019-03-22 01:59:49 +0100

    Changed the footer a little bit (show curr question num of max num)

--- Day_WEBSITE/Quiz.js
+++ Day_WEBSITE/Quiz.js
@@ -12,15 +12,29 @@ function populate() {
     for (var i = 0; i < choices.length; i++) {
       var element = document.getElementById("choice" + i);
       element.innerHTML = choices[i];
+      guess("btn" + i, choices[i]);
     }
+    showProgress();
   }
+};
+function  guess(id, guess){
+  var button= document.getElementById(id);
+  button.onclick = function(){
+    quiz.guess();
+    populate();
+  }
+};
+function showProgress(){
+  var currentQuestionNumber = quiz.questionIndex+1;
+  var element = document.getElementById("progress");
+  element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
 };
 function showScores(){
   var gameOverHtml = "<h1>Result</h1>";
   gameOverHtml += "<h2 id='score'> Your score: " +quiz.score + "</h2>";
   var element = document.getElementById("quiz");
   element.innerHTML= gameOverHtml;
-}
+};
 
 var questions = [
   new Question("Easy Question A?", ["Answer A", "Answer B", "Answer C", "Answer D"], "A"),
