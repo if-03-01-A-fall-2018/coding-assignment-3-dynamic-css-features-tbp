2907a22abeac4522447fe34953c64295947b09e6
FunnySkillz <sebastijan.bogdan@outlook.com>
2019-04-12 13:17:27 +0200

    Deleted the old quiz.

--- Day_WEBSITE/Quiz_Question.js
+++
@@ -1,9 +0,0 @@
-function Question(text, choices, answers){
-  this.text = text;
-  this.choices = choices;
-  this.answers = answers;
-}
-
-Question.prototype.correctAnswer = function(choice){
-  return choice === this.answer;
-}
